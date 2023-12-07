d9fe587dd60e07290929106c6b6873ea268fd63b
Mario Laiseca-Ruiz <mariolaisecaruiz@gmail.com>
2023-12-07 13:22:38 -0600

    adding REST api

--- app/analytics/views.py
+++ app/analytics/views.py
@@ -1,7 +1,16 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets
+
+from .serializers import HeroSerializer
+from .models import Hero
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello, world! You are in the analytics app index.")
+
+
+class HeroViewSet(viewsets.ModelViewSet):
+    queryset = Hero.objects.all().order_by('name')
+    serializer_class = HeroSerializer
